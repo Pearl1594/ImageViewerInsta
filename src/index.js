31a5f567ff99099b5bf661decc423a18ad4851b3
bharathtakkalki <bharathtakkalki@gmail.com>
2019-07-08 18:45:52 +0530

    Initial Commit
    -Created basic folder structure for the project.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <span>
+        wasssup ?
+        
+    </span>, 
+    document.getElementById('root')
+);
