f7a4bf12306e55cc327c05526d11987c3cfdc5d0
bharathtakkalki <bharathtakkalki@gmail.com>
2019-08-29 15:09:10 +0530

    Added comment In all the files - Added Comments in the Header js & css - Added Comments in Home js & css - Added comments in Controller.js - Cleared of warning by adding condition while calling api in home page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -6,20 +6,21 @@ import Login from './login/Login';
 
 
 
-
+//Creating controller class for easy routing the pages
 class Controller extends Component{
     constructor(){
         super()
-        this.baseUrl = "https://api.instagram.com/v1/users/self/";
+        this.baseUrl = "https://api.instagram.com/v1/users/self/"; //setting the baseUrl of the api 
     }
 
     render(){
         return(
             <Router>
                 <div className = 'main-container'>
-                    <Route path = '/home' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/> }/>
-                    <Route path = '/profile' render={(props) => <Profile {...props} baseUrl = {this.baseUrl}/>}/>
-                    <Route path = '/login' render={(props) => <Login {...props} baseUrl = {this.baseUrl}/>}/>
+                    
+                    <Route path = '/home' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/> }/>            {/* Route to home Page */ }
+                    <Route path = '/profile' render={(props) => <Profile {...props} baseUrl = {this.baseUrl}/>}/>       {/* Route to profile Page */ }
+                    <Route path = '/login' render={(props) => <Login {...props} baseUrl = {this.baseUrl}/>}/>           {/* Route to login Page */ }
                 </div>
             </Router>
         )
