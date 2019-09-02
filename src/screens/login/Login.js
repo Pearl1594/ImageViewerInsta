925e772d8403776764b657cfb0d1f2f0dbddb69d
MeAmatuer <sandipan2602@gmail.com>
2019-09-02 07:35:44 +0530

    Made changes to show login view in Card form. Added styling as specified.

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react';
-import { Typography } from '@material-ui/core';
 import Header from "../../common/header/Header";
-
+import './Login.css';
+import Card from "@material-ui/core/Card";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 
 class Login extends Component {
 
-
+    constructor() {
+        super();
+        this.state = {
+            usernameRequired: "dispNone",
+            passwordRequired: "dispNone",
+            incorrectUsernamePasswordMessage: "dispNone",
+            username: "",
+            password: "",
+            isLoggedIn: false,
+        };
+    }
 
     render() {
         return (
             <div>
-                <Header></Header>
-                <Typography> This is Login page </Typography>
+                
+                    <div>
+                        <Header></Header>
+                        <Card className="login-card">
+                            <p className="login-header">LOGIN</p>
+                            <FormControl required>
+                                <InputLabel htmlFor="username">Username</InputLabel>
+                                <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameChangeHandler} />
+                                <FormHelperText className={this.state.usernameRequired}><span className="red">required</span></FormHelperText>
+                            </FormControl>
+                            <br />
+                            <br />
+                            <FormControl required>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input id="password" type="password" password={this.state.password} onChange={this.inputPasswordChangeHandler} />
+                                <FormHelperText className={this.state.passwordRequired}><span className="red">required</span></FormHelperText>
+                            </FormControl>
+                            <br />
+                            <br />
+                            <FormHelperText className={this.state.incorrectUsernamePasswordMessage}><span className="red" style={{ fontSize: "14px" }}>Incorrect username and/or password</span></FormHelperText>
+                            <br />
+                            <Button variant="contained" color="primary" onClick={this.loginClickHandler} className="login-btn">LOGIN</Button>
+
+                        </Card>
+                    </div>
+
             </div>
-        )
+
+
+        );
     }
 }
 
